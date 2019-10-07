95c5461b9cf234d881ec1d39bb8cab18883e3ebf
Renat Gubaev <manhattan.doctor@mail.ru>
2019-10-07 18:42:28 +0300

    no message

--- common/dto/Paginable.ts
+++ common/dto/Paginable.ts
@@ -6,6 +6,15 @@ import { IPaginable } from './IPaginable';
 export class Paginable<U> extends Filterable<U> implements IPaginable<U> {
     // --------------------------------------------------------------------------
     //
+    //  Constants
+    //
+    // --------------------------------------------------------------------------
+
+    public static DEFAULT_PAGE_SIZE = 10;
+    public static DEFAULT_PAGE_INDEX = 0;
+
+    // --------------------------------------------------------------------------
+    //
     //  Public Static Methods
     //
     // --------------------------------------------------------------------------
@@ -15,8 +24,8 @@ export class Paginable<U> extends Filterable<U> implements IPaginable<U> {
             return item;
         }
         item = Filterable.transform(item) as IPaginable<U>;
-        item.pageSize = !_.isNil(item.pageSize) ? parseInt(item.pageSize.toString(), 10) : 10;
-        item.pageIndex = !_.isNil(item.pageIndex) ? parseInt(item.pageIndex.toString(), 10) : 0;
+        item.pageSize = !_.isNil(item.pageSize) ? parseInt(item.pageSize.toString(), 10) : Paginable.DEFAULT_PAGE_SIZE;
+        item.pageIndex = !_.isNil(item.pageIndex) ? parseInt(item.pageIndex.toString(), 10) : Paginable.DEFAULT_PAGE_INDEX;
         return item;
     }
 
