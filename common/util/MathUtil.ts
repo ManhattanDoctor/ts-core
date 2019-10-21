e83ac142e40dcf95670552213addde4aaf457bb2
Renat Gubaev <manhattan.doctor@mail.ru>
2019-10-21 19:06:14 +0300

    no message

--- common/util/MathUtil.ts
+++ common/util/MathUtil.ts
@@ -4,7 +4,37 @@ import * as _ from 'lodash';
 export class MathUtil {
     // --------------------------------------------------------------------------
     //
-    // 	Basic Methods
+    // 	Private Static Mehods
+    //
+    // --------------------------------------------------------------------------
+
+    private static _config: Decimal.Config;
+
+    // --------------------------------------------------------------------------
+    //
+    // 	Static Properties
+    //
+    // --------------------------------------------------------------------------
+
+    public static get config(): Decimal.Config {
+        return MathUtil._config;
+    }
+
+    public static set config(value: Decimal.Config) {
+        if (value === MathUtil._config) {
+            return;
+        }
+        Decimal.set(value);
+        MathUtil._config = value;
+    }
+
+    public static create(): Decimal.Constructor {
+        return Decimal.set(MathUtil.config || { defaults: true });
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    // 	Static Methods
     //
     // --------------------------------------------------------------------------
 
@@ -12,57 +42,64 @@ export class MathUtil {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             return null;
         }
-        return MathUtil.toString(new Decimal(first).add(new Decimal(second)));
+        let constructor = MathUtil.create();
+        return MathUtil.toString(new constructor(first).add(new constructor(second)));
     }
 
     public static subtract(first: string, second: string): string {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             return null;
         }
-        return MathUtil.toString(new Decimal(first).sub(new Decimal(second)));
+        let constructor = MathUtil.create();
+        return MathUtil.toString(new constructor(first).sub(new constructor(second)));
     }
 
     public static multiply(first: string, second: string): string {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             return null;
         }
-        return MathUtil.toString(new Decimal(first).mul(new Decimal(second)));
+        let constructor = MathUtil.create();
+        return MathUtil.toString(new constructor(first).mul(new constructor(second)));
     }
 
     public static divide(first: string, second: string): string {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             return null;
         }
-        return MathUtil.toString(new Decimal(first).dividedBy(new Decimal(second)));
+        let constructor = MathUtil.create();
+        return MathUtil.toString(new constructor(first).dividedBy(new constructor(second)));
     }
 
     public static floor(value: string): string {
         if (MathUtil.isInvalid(value)) {
             return null;
         }
-        return MathUtil.toString(new Decimal(value).floor());
+
+        let constructor = MathUtil.create();
+        return MathUtil.toString(new constructor(value).floor());
     }
 
     public static toString(value: Decimal.Value): string {
-        let constructor = Decimal.set({ toExpNeg: -20 });
         if (MathUtil.isInvalid(value)) {
             return null;
         }
-        return new constructor(value).valueOf();
+        return value.toString();
     }
 
     public static toNumber(value: Decimal.Value): number {
         if (MathUtil.isInvalid(value)) {
             return null;
         }
-        return new Decimal(value).toNumber();
+        let constructor = MathUtil.create();
+        return new constructor(value).toNumber();
     }
 
     public static toHex(value: Decimal.Value): string {
         if (MathUtil.isInvalid(value)) {
             return null;
         }
-        return new Decimal(value).toHex();
+        let constructor = MathUtil.create();
+        return new constructor(value).toHex();
     }
 
     public static isInvalid(value: Decimal.Value): boolean {
@@ -93,7 +130,8 @@ export class MathUtil {
         if (MathUtil.isInvalid(value)) {
             return null;
         }
-        return MathUtil.toString(new Decimal(value).abs());
+        let constructor = MathUtil.create();
+        return MathUtil.toString(new constructor(value).abs());
     }
 
     // --------------------------------------------------------------------------
@@ -106,34 +144,39 @@ export class MathUtil {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             throw new Error(`Invalid arguments`);
         }
-        return new Decimal(first).lessThan(new Decimal(second));
+        let constructor = MathUtil.create();
+        return new constructor(first).lessThan(new constructor(second));
     }
 
     public static lessThanOrEqualTo(first: string, second: string): boolean {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             throw new Error(`Invalid arguments`);
         }
-        return new Decimal(first).lessThanOrEqualTo(new Decimal(second));
+        let constructor = MathUtil.create();
+        return new constructor(first).lessThanOrEqualTo(new constructor(second));
     }
 
     public static greaterThan(first: string, second: string): boolean {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             throw new Error(`Invalid arguments`);
         }
-        return new Decimal(first).greaterThan(new Decimal(second));
+        let constructor = MathUtil.create();
+        return new constructor(first).greaterThan(new constructor(second));
     }
 
     public static greaterThanOrEqualTo(first: string, second: string): boolean {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             throw new Error(`Invalid arguments`);
         }
-        return new Decimal(first).greaterThanOrEqualTo(new Decimal(second));
+        let constructor = MathUtil.create();
+        return new constructor(first).greaterThanOrEqualTo(new constructor(second));
     }
 
     public static equals(first: string, second: string): boolean {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             throw new Error(`Invalid arguments`);
         }
-        return new Decimal(first).equals(new Decimal(second));
+        let constructor = MathUtil.create();
+        return new constructor(first).equals(new constructor(second));
     }
 }
